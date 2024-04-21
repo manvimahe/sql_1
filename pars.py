c6d613c327eed787275a10e13e66b2bf62692f31
manvimahe <manvimahe@gmail.com>
2024-04-21 16:39:28 +0530

    output is now recorded in a csv file parsed_output.csv

--- pars.py
+++ pars.py
@@ -8,7 +8,7 @@ with open(r'C:\Users\LENOVO\Desktop\sql_1\sql_1\output2.csv', 'r', newline='') a
     reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
     
     for row in reader:
-        # # print (row) 
+        # print (row, ) 
         # # row1=(', '.join(row))
         # print(row[-1])
         # statements = sqlparse.split(row[-1])
@@ -19,18 +19,34 @@ with open(r'C:\Users\LENOVO\Desktop\sql_1\sql_1\output2.csv', 'r', newline='') a
 
 ##for the queries --
 
-print("parsing on sample queries  : \n\n")
+# print("parsing on sample queries  : \n\n")
 
-for i in range(-1,-20,-1):
-    print (statements[i],'\n')
-    p=Parser(statements[i])
-    if ('INSERT' in statements[i]):
-        print('Values Inserted :- \n',p.values, '\n\n')
 
-    print('Columns Used :- \n',p.columns, '\n\n')
-    print('Tables Used :- \n',p.tables, '\n\n')
-    print('Subqueries Used :- \n',p.subqueries, '\n\n')
-    print('Column aliases Used :- \n',p.columns_aliases, '\n\n')
+with open(r'parsed_data.csv', 'w', newline='') as outfile : 
+    parsed_data=[['values inserted', 'Columns Used ', 'Tables Used ', ' Tables Used ',' Column aliases Used ']]
+    
+    for i in range(4, len(statements)):
+        # print (statements[i],'\n')
+        p=Parser(statements[i])
+        data=[]
+        if ('INSERT' in statements[i]):
+            # print('Values Inserted :- \n',p.values, '\n\n')
+            data.append(p.values)
+        else:
+            data.append(' ')
+        data.append(p.columns)
+        data.append(p.tables)
+        data.append(p.subqueries)
+        data.append(p.columns_aliases)
+        parsed_data.append(data)
+
+    writer = csv.writer(outfile)
+    writer.writerows(parsed_data)
+
+        # print('Columns Used :- \n',p.columns, '\n\n')
+        # print('Tables Used :- \n',p.tables, '\n\n')
+        # print('Tables Used :- \n',p.subqueries, '\n\n')
+        # print('Column aliases Used :- \n',p.columns_aliases, '\n\n')
 
 
 # raw = 'select * from foo; select * from bar;'
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print (statements[i],'\n')
